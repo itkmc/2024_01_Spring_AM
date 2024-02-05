5c30f17aaf402810c34c37e2437b8942848d9791
itkmc <itkang0219@gmail.com>
2024-02-05 16:50:50 +0900

    사용자의 list에 선택지 추가, boardid를 불러와서 각 게시판을 보여줄 수 있도록 변경

---
+++ src/main/java/com/example/demo/vo/Board.java
@@ -0,0 +1,17 @@
+package com.example.demo.vo;
+
+import java.sql.Timestamp;
+
+public class Board {
+	private int board_no;
+	private String board_title;
+	private String board_writer;
+	private String board_content;
+	private int board_hit;
+	private int board_like;
+	private int board_hate;
+	private String board_img;
+	private String board_img_path;
+	private int board_type;
+	private Timestamp board_regdate;
+}
