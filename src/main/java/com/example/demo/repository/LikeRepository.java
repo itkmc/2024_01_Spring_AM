f406e62bd36cb5101cdaae2148dc242ff386a19d
itkmc <itkang0219@gmail.com>
2024-02-14 13:23:10 +0900

    article vo에 good,bad,sumreactionPoint 추가, JOIN 필요

--- src/main/java/com/example/demo/repository/LikeRepository.java
+++
@@ -1,9 +0,0 @@
-import org.springframework.stereotype.Repository;
-
-@Repository
-public class LikeRepository {
-
-    public void save(int userId, int contentId) {
-        // 여기에 데이터베이스에 좋아요 정보를 저장하는 로직을 작성합니다.
-    }
-}
